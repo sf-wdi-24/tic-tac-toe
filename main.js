dcdfb7764e014bdde8a17d6a24bd90d54dbc4748
mrockway <mrockway@gmail.com>
2015-10-30 16:02:35 -0700

    working board, not tracking moves

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 // wait for DOM to load before running JS
-$(function() {
-  
-  // your code here
+$(document).ready(function () {
+    
+    // Selecting a game square
+   var turn = 0;
+	$('.gameBox').click(function (event) {
+		turn+= 1;
+		if (turn % 2 === 0){
+			$(this).addClass('playX');
+			$(this).text('X');
+			$(this).off('click'); //Disable mouse click
+			return turn;
+		}
+		else {
+			$(this).addClass('playO');
+			$(this).text('O');
+			$(this).off('click'); //Disable mouse click
+			return turn;
+		}
+	});
 
-});
+	
+
+}); //End of Ready function
