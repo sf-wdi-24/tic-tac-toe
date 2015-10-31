56eec9ca738ff8224d9d7f920876497f736ee106
mrockway <mrockway@gmail.com>
2015-10-30 22:32:08 -0700

    tracking moves into array

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ $(document).ready(function () {
     
     // Selecting a game square
    var turn = 0;
+   var moveSquare;
+   var xMoves = [];
+   var oMoves = [];
 	$('.gameBox').click(function (event) {
 		turn+= 1;
 		if (turn % 2 === 0){
 			$(this).addClass('playX');
 			$(this).text('X');
-			$(this).off('click'); //Disable mouse click
-			return turn;
+			$(this).off('click'); //Disable mouse clicks
+			moveSquare = $('.gameBox').index(this);
+			xMoves.push(moveSquare);
+			console.log(xMoves);
+			return turn, moveSquare, xMoves;
 		}
 		else {
 			$(this).addClass('playO');
 			$(this).text('O');
-			$(this).off('click'); //Disable mouse click
-			return turn;
+			$(this).off('click'); //Disable mouse clicks
+			moveSquare = $('.gameBox').index(this);
+			oMoves.push(moveSquare);
+			console.log(oMoves);
+			return turn, moveSquare, oMoves;
 		}
 	});
 
-	
 
 }); //End of Ready function
