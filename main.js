a57e6cab8ee232b1529c038b770a85bac3bb484c
mrockway <mrockway@gmail.com>
2015-11-01 00:28:37 -0700

    alternate player no replay square reset button

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
 // wait for DOM to load before running JS
 $(document).ready(function () {
-    
-    // Selecting a game square
-   var turn = 0;
-   var moveSquare;
-   var xMoves = [];
-   var oMoves = [];
-	$('.gameBox').click(function (event) {
-		turn+= 1;
-		if (turn % 2 === 0){
-			$(this).addClass('playX');
-			$(this).text('X');
-			$(this).off('click'); //Disable mouse clicks
-			moveSquare = $('.gameBox').index(this);
-			xMoves.push(moveSquare);
-			console.log(xMoves);
-			return turn, moveSquare, xMoves;
-		}
-		else {
-			$(this).addClass('playO');
-			$(this).text('O');
-			$(this).off('click'); //Disable mouse clicks
-			moveSquare = $('.gameBox').index(this);
-			oMoves.push(moveSquare);
-			console.log(oMoves);
-			return turn, moveSquare, oMoves;
-		}
-	});
+	//Setting up Variables
+	var winningArray = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
+   console.log(winningArray);
+  var turn = 0;
+ 	var moveSquare;
+ 	var xMoves = [];
+  var oMoves = [];
+  //End of Variables
 
+  //Selecting a game square
+  $('.gameBox').click(function (event){
+   	if (turn % 2 === 0){
+  		if ($(this).hasClass('playX')){
+  			console.log('hasclass playX');
+  			return;
+  		} // hasclass playX
+  		else if ($(this).hasClass('playO')){
+  			console.log('hasclass playo');
+  			return;
+  		} // hasclass playO
+  		else {
+  			$(this).addClass('playX');
+				$(this).text('X');
+				moveSquare = $('.gameBox').index(this);
+				xMoves.push(moveSquare);
+				xMoves.sort();
+				turn++;
+  		} //end playX set square
+  	} //end x turn
+  	else {
+  		if ($(this).hasClass('playX')){
+				console.log('has class playx 2');
+  			return;
+  		} // hasclass playX
+  		else if ($(this).hasClass('playO')){
+  			console.log('has class play0 2');
+  		} // hasclass playO
+  		else {
+  			$(this).addClass('playO');
+				$(this).text('O');
+				moveSquare = $('.gameBox').index(this);
+				oMoves.push(moveSquare);
+				oMoves.sort();
+				turn++;
+  		} //end playO set square	
+  	}
+  		
+  }); // Selecting a game square
 
-}); //End of Ready function
+  //Reset Button
+  $('.reset').click(function (event){
+  	$('.gameBox').removeClass('playO');
+  	$('.gameBox').removeClass('playX');
+  	$('.gameBox').empty();
+  });
+
+}); // end of document
