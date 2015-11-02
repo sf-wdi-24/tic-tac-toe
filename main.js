3bd19ae88976e4f79ecc65d544c344c7c25ffb92
vframbach <vframbach@gmail.com>
2015-11-01 21:52:56 -0800

    add player turn and colors

--- main.js
+++ main.js
@@ -5,53 +5,30 @@ $('document').ready(function() {
   
   // your code here
   $( '.box' ).click(function() {
-    console.log($(this));
+    // prevents multiple moves in same box
     if ($(this).text() !== "") {
       return;
     }
 
+    // displays x or o in current box
     $(this).text(currentPlayer);
+    $(this).addClass(currentPlayer);
+
+    // switches between x and o
     if (currentPlayer == "x") {
-      currentPlayer= "o";
+      currentPlayer = "o";
     } else {
       currentPlayer = "x";
     }
+    $('.current-player-turn').text(currentPlayer);
   });
 
   $( '.reset-button' ).click(function() {
-    $('.box').empty(); 
+    $('.box').empty();
+    $('.box').removeClass('x');
+    $('.box').removeClass('o');
+    currentPlayer = "x";   //first player will always be x
+    $('.current-player-turn').text(currentPlayer);
   });
-
-  // function getMove() {
-  // console.log("Let's play tic tac toe!");
-  //  var playerOne = 0;
-  //  var playerTwo = 0;
-  //  for (var = 0; i <= 9; i++);
-  //      var getMove(move);
-  //      if (move % 2 === 0) {
-  //      return "x";
-  //      } else {
-  //      console.log(move);
-  //      return "o";
-  //      }
-  //    if (move < 9); 
-  //      console.log("Next player, make your move");
-  //  } else {
-  //      console.log("Game over!");
-  //  }
-
-  // }
-  // any box is clicked 
-  // return "move", alternate moves between x and o
-  // only allow 1 move per box; then run jquery script to block clicks
-  // keep looping until "winning move", then return "you win!"
-  // winning moves: 
-    // 1,2,3
-    // 4,5,6
-    // 7,8,9
-    // 1,4,7
-    // 2,5,8
-    // 3,6,9
-    // 3,5,7
 
 });
