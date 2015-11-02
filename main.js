c8ebc61d2f9aab8bc2f6b38b6868cb7715ac1da0
alanlee35 <alanblee12@gmail.com>
2015-11-02 10:03:40 -0800

    the dirty way reset button works

--- main.js
+++ main.js
@@ -34,10 +34,11 @@ $(function() {
     $('.box').empty();
     $('.box').html('<p>+</p>')
     play();
+    playMore();
   })
 
-play();
-/**
+
+/** going to use winArray to refactor the ugly way of
   function getWinner(xORo) {
 		for (i = 0; < winArray.length; i++) {
 			var possibleWins = winArray[i];
@@ -57,6 +58,7 @@ play();
 
 //get winner
 //very dirty way 
+function playMore(){
 var x = 'x';
 var o = 'o';
 var spot1 = $('#a');
@@ -82,6 +84,7 @@ $('.box').on('click', function() {
 		alert('winner is ' + o )
 	  $('.box').removeClass('o');
     $('.box').removeClass('x');
+    $('.box').off();
 	} else if (spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
 		spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
 		spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
@@ -94,7 +97,49 @@ $('.box').on('click', function() {
 		alert('winner is ' + x );
 		$('.box').removeClass('o');
     $('.box').removeClass('x');
+    $('.box').off();
 	}
 })
+}
+// var x = 'x';
+// var o = 'o';
+// var spot1 = $('#a');
+// var spot2 = $('#b');
+// var spot3 = $('#c');
+// var spot4 = $('#d');
+// var spot5 = $('#e');
+// var spot6 = $('#f');
+// var spot7 = $('#g');
+// var spot8 = $('#h');
+// var spot9 = $('#i');
 
+// $('.box').on('click', function() {
+// 	if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
+// 		spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
+// 		spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
+// 		spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
+// 		spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
+// 		spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
+// 		spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
+// 		spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o')
+// 		) {
+// 		alert('winner is ' + o )
+// 	  $('.box').removeClass('o');
+//     $('.box').removeClass('x');
+// 	} else if (spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
+// 		spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
+// 		spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
+// 		spot1.hasClass('x') && spot4.hasClass('x') && spot7.hasClass('x') ||
+// 		spot2.hasClass('x') && spot5.hasClass('x') && spot8.hasClass('x') ||
+// 		spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
+// 		spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
+// 		spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x')
+// 		) {
+// 		alert('winner is ' + x );
+// 		$('.box').removeClass('o');
+//     $('.box').removeClass('x');
+// 	}
+// })
+play();
+playMore();
 });
