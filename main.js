fd7b49a72ca3d13ea0563d25a72fc2ddee66c7f9
alanlee35 <alanblee12@gmail.com>
2015-11-02 06:12:40 -0800

    added button clicks to add x and o

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 // wait for DOM to load before running JS
 $(function() {
   
-  // your code here
+	var turnCount = 0;
+
+	$('.box').on('click', function() {
+		turnCount++;
+		if (turnCount % 2 === 0) {
+			$(this).html('<p>X</p>');
+			$(this).attr('disabled','disabled');
+		}else {
+			$(this).html('<p>O</p>')
+			$(this).attr('disabled','disabled');
+		}
+	})
+
 
 });
