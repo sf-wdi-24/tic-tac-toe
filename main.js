ae9cffeac89c724206b61348b8f1eba2640ff15b
alanlee35 <alanblee12@gmail.com>
2015-11-02 07:07:25 -0800

    working on determining winner

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 $(function() {
   
 	var turnCount = 0;
+	var winArray = [[0,1,2],[0,3,6],[2,5,8],[6,7,8],[0,4,8],[2,4,6],[1,4,7],[3,4,5]];
 
 	// $('.box').on('click', function() {
 	// 	turnCount++;
@@ -13,6 +14,7 @@ $(function() {
 	// 		$(this).off('click');
 	// 	}
 	// })
+
 	function play (event) {
 		$('.box').on('click', function() {
 			turnCount++;
@@ -31,10 +33,14 @@ $(function() {
     $('.box').html('<p>+</p>')
     play();
   })
+
 play();
 /**
   function getWinner(move) {
-	
+		for (i = 0; < winArray.length; i++) {
+			var possibleWins = winArray[i];
+			var spotCount = 0;
+			for (j = 0 < )
   }
 **/
 
