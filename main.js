79aa2bbf05427a4087c3a2654133713f61e26478
abeytaadam <measalion@gmail.com>
2015-11-02 09:13:59 -0800

    Fixed clicks

--- main.js
+++ main.js
@@ -8,30 +8,34 @@ var count = 0;
 function turn() {
 	count++;
 	if (count % 2 === 0){
-		$('#turnMess').text("You're up, player O!");
-		return "X";
-	} else {
 		$('#turnMess').text("You're up, player X!");
 		return "O";
+	} else {
+		$('#turnMess').text("You're up, player O!");
+		return "X";
 	}
 }
 
 //Placing player "X" or "O" on board
 function player (event){
 	var place = turn();
+	$(this).off('click');
 	$(this).text(place);
-	// $(this).off('click');
+	
 }
 
 //Resetting board
 function clearBoard(){
+	count = 0;
+	$('.box').off('click');
 	$('.box').text('');
 	$('#turnMess').text('');
+	$('.box').click(player);
 
 }
 
 //Handling space and reset button clicks
-$('.box').click(player);
-$('input').click(clearBoard);
 
+$('input').click(clearBoard);
+clearBoard();
 });
