b70297a9114fe2b71e6a1411428250ef6e6f92ff
alanlee35 <alanblee12@gmail.com>
2015-11-02 06:54:47 -0800

    reset button works properly

--- main.js
+++ main.js
@@ -3,21 +3,39 @@ $(function() {
   
 	var turnCount = 0;
 
-	$('.box').on('click', function() {
-		turnCount++;
-		if (turnCount % 2 === 0) {
-			$(this).html('<p>X</p>');
-			$(this).off('click');
-		}else {
-			$(this).html('<p>O</p>');
-			$(this).off('click');
-		}
-	})
+	// $('.box').on('click', function() {
+	// 	turnCount++;
+	// 	if (turnCount % 2 === 0) {
+	// 		$(this).html('<p>X</p>');
+	// 		$(this).off('click');
+	// 	}else {
+	// 		$(this).html('<p>O</p>');
+	// 		$(this).off('click');
+	// 	}
+	// })
+	function play (event) {
+		$('.box').on('click', function() {
+			turnCount++;
+			if(turnCount % 2 === 0) {
+				$(this).html('<p>X</p>');
+				$(this).off('click');
+			}else {
+				$(this).html('<p>O</p>');
+				$(this).off('click')
+			}
+		})
+	}
 
   $('#reset').on('click',function(){
     $('.box').empty();
-    clickCount = 0;
     $('.box').html('<p>+</p>')
-  });
+    play();
+  })
+play();
+/**
+  function getWinner(move) {
+	
+  }
+**/
 
 });
