c6d2a5e8977f31d4ecaa5badaf8e87ebe50dc68d
abeytaadam <measalion@gmail.com>
2015-11-01 22:11:29 -0800

    Tic Tac Toe

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
-// wait for DOM to load before running JS
-$(function() {
-  
-  // your code here
+$(document).ready(function(){
+console.log("Sanity Check: JS is working!");
+
+
+//Determining player's turn and displaying it
+var count = 0;
+
+function turn() {
+	count++;
+	if (count % 2 === 0){
+		$('#turnMess').text("You're up, player O!");
+		return "X";
+	} else {
+		$('#turnMess').text("You're up, player X!");
+		return "O";
+	}
+}
+
+//Placing player "X" or "O" on board
+function player (event){
+	var place = turn();
+	$(this).text(place);
+	// $(this).off('click');
+}
+
+//Resetting board
+function clearBoard(){
+	$('.box').text('');
+	$('#turnMess').text('');
+
+}
+
+//Handling space and reset button clicks
+$('.box').click(player);
+$('input').click(clearBoard);
 
 });
